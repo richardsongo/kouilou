109ebc89d49e49cd07d51d4bf5356793aab3317e
richardsongo <rsongo@excellencelab.com>
2019-01-14 11:20:43 -0800

    Update kouilou.js

--- assets/js/kouilou.js
+++ assets/js/kouilou.js
@@ -1,1 +1,12 @@
+$(function() {
+  var $container = $('#js-site-header');
+  var $b = $('body');
+  $.waypoints.settings.scrollThrottle = 0;
+  $container.waypoint({
+    handler: function(e, d) {
+      $b.toggleClass('sticky', d === 'down');
+      e.preventDefault();
+    }
+  });
+});
 
